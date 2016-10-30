2a861cd4149869dae3f30ef3f3e9c235c0e0a5ad
Morgan Galpin <mgalpin@salesforce.com>
2016-10-30 14:27:49 -0700

    Added direction display to Rudder.

--- client/src/app/rudder/rudder.component.ts
+++ client/src/app/rudder/rudder.component.ts
@@ -9,21 +9,34 @@ import { RudderService } from './rudder.service';
 })
 export class RudderComponent implements OnInit {
 
-    currentRudder: number;
+    rudder: number;
+    direction: string;
 
     constructor(
         private rudderService: RudderService
     ) {}
 
     setRudder(value: number) {
-        if (value != this.currentRudder) {
-            console.log("Changing rudder value from: " + this.currentRudder + " to " + value)
-            this.currentRudder = value;
+        if (value != this.rudder) {
+            console.log("Changing rudder value from: " + this.rudder + " to " + value)
+            this.rudder = value;
+            this.updateDirection();
             this.rudderService.setRudder(value);
         }
     }
 
+    // Update the direction value based on the current rudder value.
+    updateDirection() {
+        if (this.rudder > 0) {
+            this.direction = 'Right ' + this.rudder;
+        } else if (this.rudder < 0) {
+            this.direction = 'Left ' + -this.rudder;
+        } else {
+            this.direction = 'Center'
+        }
+    }
+
     ngOnInit() {
-        this.currentRudder = 0;
+        this.setRudder(0);
     }
 }
